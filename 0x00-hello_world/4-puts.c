87f327ffc3e889be8c862c7d4cdc7786a9e2f3ed
Velilemabaso <velilemabaso98@gmail.com>
2023-07-13 03:49:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle,followed by a new line.
+ * puzzle,followed by a newline.
  * Return: Always 0.
  */
 
@@ -10,6 +10,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }	
