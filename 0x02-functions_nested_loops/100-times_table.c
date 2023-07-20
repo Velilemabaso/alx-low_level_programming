57f41d5f5821fea008cfff4fa1ec5a592a12f155
Velilemabaso <velilemabaso98@gmail.com>
2023-07-20 12:53:12 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,27 +7,27 @@
  */
 void print_times_table(int n)
 {
-    if (n >= 0 && n <= 15)
-    {
-        int row, column, result;
+if (n >= 0 && n <= 15)
+{
+int row, column, result;
 
-        for (row = 0; row <= n; row++)
-        {
-            for (column = 0; column <= n; column++)
-            {
-                result = row * column;
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+result = row * column;
 
-                if (column == 0)
-                    printf("%d", result);
-                else if (result < 10)
-                    printf(",   %d", result);
-                else if (result < 100)
-                    printf(",  %d", result);
-                else
-                    printf(", %d", result);
-            }
-            printf("\n");
-        }
-    }
+if (column == 0)
+printf("%d", result);
+else if (result < 10)
+printf(",   %d", result);
+else if (result < 100)
+printf(",  %d", result);
+else
+printf(", %d", result);
+}
+printf("\n");
+}
+}
 }
 
